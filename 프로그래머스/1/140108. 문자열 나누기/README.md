25e82c5fa6df650fa21e8dec6a8bda205b2f5e39
Lamool <100755786+Lamool@users.noreply.github.com>
2025-01-31 23:49:53 +0900

    [level 1] Title: 문자열 나누기, Time: 0.92 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 31.11 ms
+메모리: 90.3 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 31일 23:20:24
+2025년 01월 31일 23:49:51
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,31 +1,25 @@
 class Solution {
+
     public int solution(String s) {
-        int answer = 1;
-        
-        char strFirst = s.charAt(0);    // 문자열 첫 번째 글자를 담을 변수
-        
-        int xNum = 0;       // x 글자 수를 담을 변수
-        int oNum = 0;       // x가 아닌 다른 글자 수를 담을 변수
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == strFirst) {
-                xNum++;
+        char prev = '1';
+        int same = 0, different = 0, answer = 0;
+        for (char c : s.toCharArray()) {
+            if (prev == '1') {
+                prev = c;
+                same++;
+                answer++;
+            } else if (prev == c) {
+                same++;
             } else {
-                oNum++;
+                different++;
             }
-            
-            if (xNum == oNum) {
-                if (i + 1 < s.length()) {
-                    s = s.substring(i + 1);
-                    strFirst = s.charAt(0);
-                    xNum = 0;
-                    oNum = 0;
-                    answer++;
-                    i = -1;
-                }
+
+            if (same == different) {
+                prev = '1';
+                same = 0; different = 0;
             }
         }
-        
+
         return answer;
     }
 }
