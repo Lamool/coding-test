5c9ff0c47584fa2d622f41fdf18cc2b3ecb3df7b
Lamool <100755786+Lamool@users.noreply.github.com>
2025-01-31 23:52:07 +0900

    [level 1] Title: 문자열 나누기, Time: 31.11 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,25 +1,31 @@
 class Solution {
-
     public int solution(String s) {
-        char prev = '1';
-        int same = 0, different = 0, answer = 0;
-        for (char c : s.toCharArray()) {
-            if (prev == '1') {
-                prev = c;
-                same++;
-                answer++;
-            } else if (prev == c) {
-                same++;
+        int answer = 1;
+        
+        char strFirst = s.charAt(0);    // 문자열 첫 번째 글자를 담을 변수
+        
+        int xNum = 0;       // x 글자 수를 담을 변수
+        int oNum = 0;       // x가 아닌 다른 글자 수를 담을 변수
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == strFirst) {
+                xNum++;
             } else {
-                different++;
+                oNum++;
             }
-
-            if (same == different) {
-                prev = '1';
-                same = 0; different = 0;
+            
+            if (xNum == oNum) {
+                if (i + 1 < s.length()) {
+                    s = s.substring(i + 1);
+                    strFirst = s.charAt(0);
+                    xNum = 0;
+                    oNum = 0;
+                    answer++;
+                    i = -1;
+                }
             }
         }
-
+        
         return answer;
     }
 }
