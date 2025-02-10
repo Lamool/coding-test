34a496a6b6ab942751a63646658b25c8ad8de861
Lamool <100755786+Lamool@users.noreply.github.com>
2025-02-10 23:45:51 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.25 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : my_string.toCharArray()) {
+            if (sb.indexOf(String.valueOf(c)) == -1) {
+                sb.append(c);
+            }
+        }
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
