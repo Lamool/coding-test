8ceba73603926ef169d03cdef40ecf8e35667349
Lamool <100755786+Lamool@users.noreply.github.com>
2025-03-08 03:00:22 +0900

    [level 1] Title: 햄버거 만들기, Time: 14.97 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 145 MB, 시간: 61.65 ms
+메모리: 153 MB, 시간: 14.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 08일 02:52:16
+2025년 03월 08일 03:00:20
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,44 +1,18 @@
-import java.util.Stack;
-
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-        Stack<Integer> st = new Stack<>();
-        
-        for (int i = 0; i < ingredient.length; i++) {
-            if (i >= 3) {   // 햄버거 만드려면 재료가 4개 필요해서
-                if (ingredient[i] == 1) {           // 1) 재료가 빵이라면
-                    if (!st.isEmpty() && st.peek() == 3) {           // 2) 재료가 고기라면
-                        st.pop();
-                        
-                        if (!st.isEmpty() && st.peek() == 2) {       // 3) 재료가 야채라면
-                            st.pop();
-                        
-                            if (!st.isEmpty() && st.peek() == 1) {   // 4) 재료가 빵이라면
-                                st.pop();
-                                answer++;
-                            } else {                // 4) 재료가 빵이 아니라면
-                                // 스택에서 뺀 지금까지의 재료들과 새로운 재료 순서에 맞춰 다시 스택에 push
-                                st.push(2);
-                                st.push(3);
-                                st.push(1);
-                            }
-                        } else {    // 3) 재료가 야채가 아니라면
-                            // 스택에서 뺀 지금까지의 재료들과 새로운 재료 순서에 맞춰 다시 스택에 push
-                            st.push(3);
-                            st.push(1);
-                        }
-                    } else {        // 2) 재료가 고기가 아니라면
-                        st.push(1);
-                    }
-                } else {            // 1) 재료가 빵이 아니라면
-                    st.push(ingredient[i]);
-                }
-            } else {
-                st.push(ingredient[i]);
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
             }
-        }   // for end
-        
+        }
         return answer;
     }
 }
