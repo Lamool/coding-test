7117626b29e191093952400eebe274f1e8c9b6ee
Lamool <100755786+Lamool@users.noreply.github.com>
2025-03-31 23:25:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        int num = 0;
-        int rest = 0;
-        
-        while (true) {
-            num = n / a * b;
-            answer += n / a * b;
-            rest = n % a;
-            n = num + rest;
-                        
-            if (n < a) break;
-        }
-        
-        return answer;
+        return (n > b ? n - b : 0) / (a - b) * b;
     }
 }
