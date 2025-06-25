2ea48b9fd4976e5a6c1af14319ab9c87e94ab1dd
Lamool <100755786+Lamool@users.noreply.github.com>
2025-06-25 23:57:19 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        answer = hp / 5;
+        hp %= 5;
+        if (hp == 0) return answer;
+        
+        answer += hp / 3;
+        hp %= 3;
+        if (hp == 0) return answer;
+        
+        answer += hp / 1;
+        return answer;
+    }
+}
