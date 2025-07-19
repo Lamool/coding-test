d8f7bd89b07647bdfedfd7536aa1ca41e6b04a7c
Lamool <100755786+Lamool@users.noreply.github.com>
2025-07-19 23:11:25 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for (int i = 0; i < array.Length; i++) {
+            if (n == array[i]) answer++;
+        }
+        
+        return answer;
+    }
+}
