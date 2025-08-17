f28db58dca7e429601ff5205238dcf5e251c2193
Lamool <100755786+Lamool@users.noreply.github.com>
2025-08-17 23:37:12 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.32 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] arr = control.toCharArray();
+        int i;
+        
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] == 'w') {
+                n += 1;
+            } else if (arr[i] == 's') {
+                n -= 1;
+            } else if (arr[i] == 'd') {
+                n += 10;
+            } else if (arr[i] == 'a') {
+                n -= 10;
+            }  
+        }
+        
+        
+        return n;
+    }
+}
